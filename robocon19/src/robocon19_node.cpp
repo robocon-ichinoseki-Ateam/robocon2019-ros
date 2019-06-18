01016e35bb3d4204a73566fbf912ec402d87d79a
ryoga <surpace2479@gmail.com>
2019-06-18 13:56:33 +0900

    add: ロボットの座標をPose2Dで配信する機能を追加

--- robocon19/src/robocon19_node.cpp
+++ robocon19/src/robocon19_node.cpp
@@ -2,16 +2,98 @@
 #include <geometry_msgs/PoseStamped.h>
 #include <tf/transform_listener.h>
 #include <jsk_rviz_plugins/OverlayText.h>
+#include "geometry_msgs/Pose2D.h"
 #include <string>
 #include <math.h>
 #include <iostream>
 #include <stdio.h>
 #include <sstream>
-
 #include <string>
 #include <math.h>
 
-void sendText(jsk_rviz_plugins::OverlayText &t, std::string str)
+void geometry_quat_to_rpy(double &roll, double &pitch, double &yaw, geometry_msgs::Quaternion geometry_quat);
+void getRobotPose(float return_pose[3]);
+void configOverlayText(jsk_rviz_plugins::OverlayText &t, std::string str);
+std::string generateDisplayStr(float pose[3]);
+
+int main(int argc, char **argv)
+{
+    ros::init(argc, argv, "robocon19_node");
+    ros::NodeHandle nh;
+
+    jsk_rviz_plugins::OverlayText text;
+    ros::Publisher text_pub = nh.advertise<jsk_rviz_plugins::OverlayText>("text", 1);
+
+    ros::Publisher pub_pose = nh.advertise<geometry_msgs::Pose2D>("robot_pose", 10);
+    geometry_msgs::Pose2D pose;
+
+    // ループ周波数を指定
+    ros::Rate loop_rate(50);
+
+    while (ros::ok())
+    {
+        // ロボットの座標を取得
+        float pose_arry[3] = {0};
+        getRobotPose(pose_arry);
+
+        // ロボットの座標をTopicとしてPublish
+        pose.x = pose_arry[0];
+        pose.y = pose_arry[1];
+        pose.theta = pose_arry[2];
+        pub_pose.publish(pose);
+
+        // 表示用文字列を生成し、ロボットの座標をrvizに表示
+        std::string send_str = generateDisplayStr(pose_arry);
+        configOverlayText(text, send_str);
+        text_pub.publish(text);
+
+        ros::spinOnce();
+        loop_rate.sleep();
+    }
+    return 0;
+}
+
+// クウォータニオンからオイラー角を返す
+void geometry_quat_to_rpy(double &roll, double &pitch, double &yaw, geometry_msgs::Quaternion geometry_quat)
+{
+    tf::Quaternion quat;
+    quaternionMsgToTF(geometry_quat, quat);
+    tf::Matrix3x3(quat).getRPY(roll, pitch, yaw); //rpy are Pass by Reference
+}
+
+// TFを読んでロボットの座標と角度を返す
+void getRobotPose(float return_pose[3])
+{
+    tf::TransformListener ln;
+
+    geometry_msgs::PoseStamped source_pose;
+    source_pose.header.frame_id = "base_link";
+    source_pose.header.stamp = ros::Time(0);
+    source_pose.pose.orientation.w = 1.0;
+
+    geometry_msgs::PoseStamped target_pose;
+    std::string target_frame = "map";
+
+    try
+    {
+        ln.waitForTransform(source_pose.header.frame_id, target_frame, ros::Time(0), ros::Duration(1.0));
+        ln.transformPose(target_frame, source_pose, target_pose);
+    }
+    catch (...)
+    {
+        ROS_INFO("tf error");
+    }
+
+    double roll, pitch, yaw;
+    geometry_quat_to_rpy(roll, pitch, yaw, target_pose.pose.orientation);
+
+    return_pose[0] = target_pose.pose.position.x;
+    return_pose[1] = target_pose.pose.position.y;
+    return_pose[2] = yaw;
+}
+
+// rvizに表示するテキストの設定
+void configOverlayText(jsk_rviz_plugins::OverlayText &t, std::string str)
 {
     t.action = jsk_rviz_plugins::OverlayText::ADD;
     t.width = 140;
@@ -38,61 +120,14 @@ void sendText(jsk_rviz_plugins::OverlayText &t, std::string str)
     t.text = str;
 }
 
-double roll, pitch, yaw;
-void geometry_quat_to_rpy(double &roll, double &pitch, double &yaw, geometry_msgs::Quaternion geometry_quat)
+// rvizに表示するテキストデータを生成
+std::string generateDisplayStr(float pose[3])
 {
-    tf::Quaternion quat;
-    quaternionMsgToTF(geometry_quat, quat);
-    tf::Matrix3x3(quat).getRPY(roll, pitch, yaw); //rpy are Pass by Reference
-}
+    std::stringstream ss;
+    ss << "-position-\r\n"
+       << "x: " << pose[0] << "\r\n"
+       << "y: " << pose[1] << "\r\n"
+       << "z: " << pose[2] * (180 / 3.14159); /*to degree*/
 
-int main(int argc, char **argv)
-{
-    ros::init(argc, argv, "robocon19_node");
-    ros::NodeHandle nh;
-
-    tf::TransformListener ln;
-
-    jsk_rviz_plugins::OverlayText text;
-    ros::Publisher text_pub = nh.advertise<jsk_rviz_plugins::OverlayText>("text", 1);
-
-    ros::Rate loop_rate(10);
-    while (ros::ok())
-    {
-        geometry_msgs::PoseStamped source_pose;
-        source_pose.header.frame_id = "base_link";
-        source_pose.header.stamp = ros::Time(0);
-        source_pose.pose.orientation.w = 1.0;
-
-        geometry_msgs::PoseStamped target_pose;
-        std::string target_frame = "map";
-        try
-        {
-            ln.waitForTransform(source_pose.header.frame_id, target_frame, ros::Time(0), ros::Duration(1.0));
-            ln.transformPose(target_frame, source_pose, target_pose);
-
-            geometry_quat_to_rpy(roll, pitch, yaw, target_pose.pose.orientation);
-
-            std::stringstream ss;
-            ss << "-position-\r\n"
-               << "x: " << target_pose.pose.position.x << "\r\n"
-               << "y: " << target_pose.pose.position.y << "\r\n"
-               << "z: " << yaw * 180 / 3.14159;
-
-            std::string send_str = ss.str();
-
-            sendText(text, send_str);
-            text_pub.publish(text);
-
-            ROS_INFO("x:%+5.2f, y:%+5.2f,z:%+5.2f", target_pose.pose.position.x, target_pose.pose.position.y, target_pose.pose.position.z);
-        }
-        catch (...)
-        {
-            ROS_INFO("tf error");
-        }
-
-        ros::spinOnce();
-        loop_rate.sleep();
-    }
-    return 0;
+    return ss.str();
 }

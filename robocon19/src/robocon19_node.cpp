70e9999f2b81dae636a204bae11c6618ccd42615
ryoga <surpace2479@gmail.com>
2019-06-24 00:28:10 +1100

    add: ラインセンサの可視化機能

--- robocon19/src/robocon19_node.cpp
+++ robocon19/src/robocon19_node.cpp
@@ -7,8 +7,9 @@ std_msgs::Float32 linear_data, angular_data;
 
 float pose_arry[3] = {0};
 float pre_pose[3] = {0};
+int line_sensor[2] = {0};
 
-void poseAMCLCallback(const geometry_msgs::PoseWithCovarianceStamped::ConstPtr &msgAMCL)
+void callbackAmcl(const geometry_msgs::PoseWithCovarianceStamped::ConstPtr &msgAMCL)
 {
     static ros::Time pre_time;
     ros::Duration ros_duration = ros::Time::now() - pre_time;
@@ -31,25 +32,96 @@ void poseAMCLCallback(const geometry_msgs::PoseWithCovarianceStamped::ConstPtr &
         pre_pose[i] = pose_arry[i];
 }
 
+void callbackLinesensor(const std_msgs::Int32MultiArray &msg_line_sensor)
+{
+    line_sensor[0] = msg_line_sensor.data[0];
+    line_sensor[1] = msg_line_sensor.data[1];
+}
+
+visualization_msgs::MarkerArray generateDisplayLinesensor(int sensor_id, float attach_pose[2], int snesor_val)
+{
+    visualization_msgs::MarkerArray m_a;
+
+    const float interval = 0.015;
+    const int s_num = 8;
+
+    m_a.markers.resize(s_num);
+
+    for (int i = 0; i < s_num; i++)
+    {
+        m_a.markers[i].header.frame_id = "/base_link";
+        m_a.markers[i].header.stamp = ros::Time::now();
+        m_a.markers[i].ns = "robocon19_node";
+        m_a.markers[i].id = i;
+        m_a.markers[i].lifetime = ros::Duration();
+
+        m_a.markers[i].type = visualization_msgs::Marker::CUBE;
+        m_a.markers[i].action = visualization_msgs::Marker::ADD;
+        m_a.markers[i].scale.x = interval;
+        m_a.markers[i].scale.y = interval;
+        m_a.markers[i].scale.z = 0.001;
+
+        if (sensor_id == 0)
+        {
+            m_a.markers[i].pose.position.x = attach_pose[0] + (float)i * interval - interval * 3.5;
+            m_a.markers[i].pose.position.y = attach_pose[1];
+        }
+        else
+        {
+            m_a.markers[i].pose.position.x = attach_pose[0];
+            m_a.markers[i].pose.position.y = attach_pose[1] + (float)i * interval - interval * 3.5;
+        }
+
+        m_a.markers[i].pose.position.z = 0;
+        m_a.markers[i].pose.orientation.x = 0;
+        m_a.markers[i].pose.orientation.y = 0;
+        m_a.markers[i].pose.orientation.z = 0;
+        m_a.markers[i].pose.orientation.w = 1;
+
+        if ((snesor_val >> i) & 1)
+        {
+            m_a.markers[i].color.r = 1.0f;
+            m_a.markers[i].color.g = 0.0f;
+            m_a.markers[i].color.b = 1.0f;
+        }
+        else
+        {
+            m_a.markers[i].color.r = 1.0f;
+            m_a.markers[i].color.g = 1.0f;
+            m_a.markers[i].color.b = 0.0f;
+        }
+
+        m_a.markers[i].color.a = 1.0f;
+    }
+
+    return m_a;
+}
+
 int main(int argc, char **argv)
 {
     ros::init(argc, argv, "robocon19_node");
     ros::NodeHandle nh;
+    ros::Rate loop_rate(100);
 
     jsk_rviz_plugins::OverlayText text;
-    ros::Publisher text_pub = nh.advertise<jsk_rviz_plugins::OverlayText>("text", 1);
+
+    ros::Subscriber sub_amcl = nh.subscribe("amcl_pose", 100, callbackAmcl);
+    ros::Subscriber sub_line_sensor = nh.subscribe("line_sensor", 100, callbackLinesensor);
 
     ros::Publisher linear_pub = nh.advertise<std_msgs::Float32>("linear_v", 10);
     ros::Publisher angular_pub = nh.advertise<std_msgs::Float32>("angular_v", 10);
-
-    // ros::Subscriber sub_pose = nh.subscribe("robot_pose", 1000, callbackFromPose);
-    ros::Subscriber sub_amcl = nh.subscribe("amcl_pose", 100, poseAMCLCallback);
-
-    // ループ周波数を指定
-    ros::Rate loop_rate(100);
+    ros::Publisher pub_line_sensor_x = nh.advertise<visualization_msgs::MarkerArray>("line_sensor_marker_x", 1);
+    ros::Publisher pub_line_sensor_y = nh.advertise<visualization_msgs::MarkerArray>("line_sensor_marker_y", 1);
+    ros::Publisher text_pub = nh.advertise<jsk_rviz_plugins::OverlayText>("text", 1);
 
     while (ros::ok())
     {
+        ROS_INFO("%d", line_sensor[0]);
+        float attach_pose[2][2] = {{0, -0.25}, {0.25, 0}};
+
+        pub_line_sensor_x.publish(generateDisplayLinesensor(0, attach_pose[0], line_sensor[0]));
+        pub_line_sensor_y.publish(generateDisplayLinesensor(1, attach_pose[1], line_sensor[1]));
+
         linear_pub.publish(linear_data);
         angular_pub.publish(angular_data);
 
@@ -61,6 +133,7 @@ int main(int argc, char **argv)
         ros::spinOnce();
         loop_rate.sleep();
     }
+
     return 0;
 }
 

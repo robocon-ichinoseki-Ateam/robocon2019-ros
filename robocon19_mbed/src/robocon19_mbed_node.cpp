a70d38f97a25700b59b8df06bc152866d8b6bc10
ryoga <surpace2479@gmal.com>
2019-10-08 21:34:54 +0900

    update: 現状

--- robocon19_mbed/src/robocon19_mbed_node.cpp
+++ robocon19_mbed/src/robocon19_mbed_node.cpp
@@ -18,14 +18,14 @@ void callbackFromMbed(const std_msgs::Float32MultiArray &msg)
     lift_height.data = msg.data[4];
     line_sensor_arry[1] = msg.data[5];
 
-    if (!init_flag)
-    {
-        if (abs(msg.data[1]) < 0.001 && abs(msg.data[2]) < 0.001)
-        {
-            needs_reset_pose.data = true;
-            init_flag = true;
-        }
-    }
+    // if (!init_flag)
+    // {
+    //     if (abs(msg.data[1]) < 0.001 && abs(msg.data[2]) < 0.001)
+    //     {
+    //         needs_reset_pose.data = true;
+    //         init_flag = true;
+    //     }
+    // }
     // measureLooptime();
 }
 
